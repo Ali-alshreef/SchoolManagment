66b85e3bf812cd05f6b58e70595d783a26b01f8d
Ali alshareef <a.alshareef218@gmail.com>
2025-05-07 14:39:51 +0200

    Complate FrmUpSertSrtudent

--- SchoolManagment/Forms/FrmUpsertStudent.cs
+++ SchoolManagment/Forms/FrmUpsertStudent.cs
@@ -1,5 +1,6 @@
 ﻿using SchoolManagment.Data;
 using SchoolManagment.Models;
+using System.Windows.Forms;
 
 namespace SchoolManagment.Forms
 {
@@ -10,10 +11,11 @@ namespace SchoolManagment.Forms
         public FrmUpsertStudent()
         {
             InitializeComponent();
+
         }
-      
+
         List<string> gernders = new List<string>();
-       
+
         void Activ(bool c)
         {
             txtId.Enabled = c;
@@ -21,29 +23,41 @@ namespace SchoolManagment.Forms
             DTPBithDate.Enabled = c;
             CBGenderType.Enabled = c;
         }
-        
+
         private void FrmUpsertStudent_Load(object sender, EventArgs e)
         {
             Activ(false);
             gernders.Add("ذكر");
             gernders.Add("أنثى");
             CBGenderType.DataSource = gernders;
+            btnSave.Enabled = false;
         }
         private void btnNew_Click(object sender, EventArgs e)
         {
             Student = new Student();
-
             Activ(true);
+            btnSave.Enabled = true;
+            btnNew.Enabled = false;
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
             try
             {
+                if (string.IsNullOrEmpty(txtFullName.Text)
+                    || string.IsNullOrWhiteSpace(txtFullName.Text))
+                {
+                    MessageBox.Show("الاسم فارغ");
+                    return;
+                }
+
                 Student.Id = Convert.ToInt16(txtId.Text);
                 Student.FullName = txtFullName.Text;
+                Student.BirthDate = DTPBithDate.Value;
                 db.Students.Add(Student);
                 Activ(false);
+                btnSave.Enabled = false;
+                btnNew.Enabled = true;
             }
             catch (Exception)
             {
